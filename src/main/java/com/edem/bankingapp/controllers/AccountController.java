bc7595a6f6cb96428d4db54bc0e1da3a94a364cf
heyEdem <akatoaugustine@gmail.com>
2024-04-06 18:15:06 +0000

    AccountController - withdraw and delete methods

--- src/main/java/com/edem/bankingapp/controllers/AccountController.java
+++ src/main/java/com/edem/bankingapp/controllers/AccountController.java
@@ -3,11 +3,11 @@ package com.edem.bankingapp.controllers;
 import com.edem.bankingapp.Dto.AccountDto;
 import com.edem.bankingapp.service.AccountService;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -35,5 +35,24 @@ public class AccountController { private final AccountService accountService;
         double amount = request.get("amount");
         AccountDto accountDto = accountService.deposit(id,amount );
         return ResponseEntity.ok(accountDto);
+    }
+
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request){
+        double amount = request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id, amount);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AccountDto>> getAll(){
+        List<AccountDto> accounts = accountService.getAllAccounts();
+        return ResponseEntity.ok(accounts);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("Success");
     }
 }
